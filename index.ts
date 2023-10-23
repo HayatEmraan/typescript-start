008e257b7dc3af7c03d1d058764add99b517d2c7
hayatemraan <emraanhasanhayat@gmail.com>
2023-10-23 20:11:43 +0600

    started - ts

---
+++ index.ts
@@ -0,0 +1,41 @@
+// // first program in ts
+
+// let x = 10;
+// let y = 20;
+
+// console.log(x + y);
+
+// // second program in ts
+
+// let firstName: string = "Dylan";
+
+// let age: number = 30;
+
+// let isSingle: boolean = true;
+
+// console.log(firstName);
+
+// third program in ts
+
+// function myFunc(z: any, a: number): number {
+//   return z + a;
+// }
+
+// console.log(myFunc(10, 20));
+
+
+// fourth program in ts
+
+let i: any = 10;
+
+i = "hello";
+i = true;
+
+console.log(i);
+
+
+let vz: null = null;
+
+let xy: undefined = undefined;
+
+console.log(vz);
